a76cd6cb14ae38d4ac3d94badcd007db70ab3ac3
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-03-28 17:26:31 +0900

    Update 직사각형에서 탈출.java

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,40 +1,24 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.Scanner;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String n = br.readLine();
+        int[] num = new int[4];
         StringTokenizer st = new StringTokenizer(n);
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
         int h = Integer.parseInt(st.nextToken());
 
+        num[0] = x;
+        num[1] = y;
+        num[2] = w-x;
+        num[3] = h-y;
+
         //x, y, w-x, h-y 여기의 최소값 찾기
-        if(x<y&&w-x<h-y){
-            if(x<w-x)
-                System.out.println(x);
-            else
-                System.out.println(w-x);
-        }
-        else if(x<y&&h-y<w-x){
-            if(x<h-y)
-                System.out.println(x);
-            else
-                System.out.println(h-y);
-        }
-        else if(y<x&&w-x<h-y){
-            if(y<w-x)
-                System.out.println(y);
-            else
-                System.out.println(w-x);
-        }
-        else{
-            if(y<h-y)
-                System.out.println(y);
-            else
-                System.out.println(h-y);
-        }
+        Arrays.sort(num);
+        System.out.println(num[0]);
     }
 }
