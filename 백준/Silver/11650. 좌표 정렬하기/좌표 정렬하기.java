00c57a4a576843885c45909d2b6b0259df47db80
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2023-11-29 13:43:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1432 ms, Memory: 56676 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,45 +9,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-class Point implements Comparable<Point> {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-
-    @Override
-    public int compareTo(Point o) {
-        if (this.x == o.x) {
-            return this.y - o.y;
-        }
-        return this.x - o.x;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        List<Point> points = new ArrayList<>();
+        int[][] points;
 
         int n = Integer.parseInt(br.readLine());
+        points = new int[n][2];
 
         for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine()," ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            points.add(new Point(x,y));
+            points[i][0] = x;
+            points[i][1] = y;
         }
 
-        points.sort(Comparator.naturalOrder());
+        Arrays.sort(points, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
 
-        for(Point p : points){
-            System.out.println(p.x + " " + p.y);
+        for(int[] p : points){
+            System.out.println(p[0] + " " + p[1]);
         }
 
     }
