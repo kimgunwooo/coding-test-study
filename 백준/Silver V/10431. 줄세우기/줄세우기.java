8c388fb6d0e03a5b0a70544353d39d8b52082c46
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2024-02-14 20:15:14 +0900

    [Silver V] Title: 줄세우기, Time: 236 ms, Memory: 19808 KB -BaekjoonHub

---
+++ 백준/Silver V/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<n;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            sb.append(st.nextToken()).append(" ");
+
+            LinkedList<Integer> students = new LinkedList<>();
+            for(int j=0;j<20;j++) {
+                students.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int result = Sort(students);
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int Sort(LinkedList<Integer> students) {
+        int result=0;
+        for(int i=1;i<20;i++) {
+            for(int j=0;j<i;j++){
+                if(students.get(j) > students.get(i)){
+                    int temp = students.remove(i);
+                    students.add(j, temp);
+                    result += (Math.abs(i-j));
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
