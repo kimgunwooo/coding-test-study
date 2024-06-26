2d8be9886fdb918f1ecb298f96650ba6e3efdf9c
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2024-06-26 10:36:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 320 ms, Memory: 26048 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,33 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static StringBuilder sb = new StringBuilder();
+	public static StringTokenizer st;
+	public static int N;
+	public static PriorityQueue<AbsNum> absHeap = new PriorityQueue<>();
 
-        PriorityQueue<Integer> absHeap = new PriorityQueue<>((o1, o2) -> {
-            if(Math.abs(o1) == Math.abs(o2)){
-                return o1-o2;
-            }
-            return Math.abs(o1)-Math.abs(o2);
-        });
+	public static class AbsNum implements Comparable<AbsNum> {
+		long x;
 
-        for(int i=0;i<n;i++){
-            int x = Integer.parseInt(br.readLine());
-            if(x!=0){
-                absHeap.add(x);
-            } else {
-                if(absHeap.isEmpty()){
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(absHeap.remove()).append("\n");
-                }
-            }
-        }
+		public AbsNum(long x) {
+			this.x = x;
+		}
 
-        System.out.println(sb);
-    }
+		@Override
+		public int compareTo(AbsNum absNum) {
+			if(Math.abs(this.x) < Math.abs(absNum.x)) {
+				return -1;
+			} else if (Math.abs(this.x) == Math.abs(absNum.x)){
+				return (int)(this.x - absNum.x);
+			} else {
+				return 1;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine()); // 연산의 개수
+
+		for(int i=0;i<N;i++) {
+			long x = Long.parseLong(br.readLine());
+			solution(x);
+		}
+
+		System.out.println(sb);
+	}
+
+	private static void solution(long x) {
+		if(x == 0) { // 가장 작은 값을 출력, 제거
+			AbsNum num = absHeap.poll();
+			if(num != null) {
+				sb.append(num.x).append("\n");
+			} else {
+				sb.append(0).append("\n");
+			}
+		} else { // 추가
+			absHeap.add(new AbsNum(x));
+		}
+	}
 }
