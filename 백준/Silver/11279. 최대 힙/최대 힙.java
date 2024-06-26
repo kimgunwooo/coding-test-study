bc37b21981040d63db118d2e115b530ac2082ecc
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2024-06-26 10:09:02 +0900

    [Silver II] Title: 최대 힙, Time: 296 ms, Memory: 26912 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,31 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static StringBuilder sb = new StringBuilder();
+	public static StringTokenizer st;
+	public static int N;
+	public static PriorityQueue<Long> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine()); // 연산의 개수
 
-        for(int i=0;i<n;i++){
-            int x = Integer.parseInt(br.readLine());
-            if(x>0){
-                maxHeap.add(x);
-            }
-            else if(x==0){
-                if(maxHeap.isEmpty()){
-                    sb.append(0).append("\n");
-                }
-                else{
-                    int max = maxHeap.remove();
-                    sb.append(max).append("\n");
-                }
-            }
-        }
+		for(int i=0;i<N;i++) {
+			long x = Long.parseLong(br.readLine());
+			solution(x);
+		}
 
-        System.out.println(sb);
-    }
+		System.out.println(sb);
+	}
+
+	private static void solution(long x) {
+		if(x == 0) { // 가장 작은 값을 출력, 제거
+			if(maxHeap.isEmpty()) {
+				sb.append(0).append("\n");
+				return;
+			}
+			long min = maxHeap.remove();
+			sb.append(min).append("\n");
+		} else { // 추가
+			maxHeap.add(x);
+		}
+	}
 }
