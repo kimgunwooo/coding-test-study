04831a7ae33d1596e526ec1f574787f3ab3d1306
Gunwoo Kim <96743351+kimgunwooo@users.noreply.github.com>
2024-11-13 10:43:36 +0900

    [level 2] Title: 테이블 해시 함수, Time: 11.79 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+/*
+
+*/
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        
+        Arrays.sort(data, (o1, o2) -> {
+            if (o1[col-1] == o2[col-1]) {
+                return o2[0] - o1[0];
+            }
+            return o1[col-1] - o2[col-1];
+        });
+        
+        // for (int i=0; i<data.length; i++) {
+        //     for (int j=0; j<data[0].length; j++) {
+        //         System.out.print(data[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        int result = 0;
+        for (int i=row_begin-1; i<row_end; i++) {
+            int temp = 0;
+            for (int j=0; j<data[0].length; j++) {
+                temp += data[i][j] % (i+1);
+            }
+            result ^= temp;
+        }
+        
+        return result;
+    }
+}
