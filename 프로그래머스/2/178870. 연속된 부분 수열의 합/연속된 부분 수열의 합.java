1bd0203596ecaba8cd96fa2f4d420855206e0f03
Gunwoo Kim <96743351+kimgunwooo@users.noreply.github.com>
2025-03-11 11:24:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 19.01 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,43 +1,39 @@
-/**
+import java.util.*;
+/*
+부분 수열 합 = k
+그 중에서 가장 길이가 짧은 수열 구하기, 시작 인덱스가 작은 것으로
+시작인덱스, 끝인덱스로 반환
+---
+투포인터 알고리즘으로 접근
 
 */
 
 class Solution {
-    public static int maxIndex;
-    public static int minIndex;
-    
     public int[] solution(int[] sequence, int k) {
-        return this.findPartialSequence(sequence, k);
-    }
-    
-    public int[] findPartialSequence(int[] sequence, int k) {
-        maxIndex = 0;
-        minIndex = 0;
-        int sum = sequence[0];
-        int length = Integer.MAX_VALUE;
-        int[] result = new int[2];
+        int[] answer = new int[2];
         
-        while (true) {
-            if (sum > k) {
-                sum -= sequence[minIndex];
-                minIndex++;
-            } else if (sum < k) {
-                if (maxIndex + 1 >= sequence.length) {
-                    break;
+        int left = 0;
+        int right = 0;
+        int sum = sequence[left];
+        int min = 1_000_001;
+        while (left <= right) {
+            if (sum < k) {
+                if (right+1 >= sequence.length) break;
+                sum += sequence[++right];
+            } else if (sum > k) {
+                sum -= sequence[left++];
+            }
+            
+            if (sum == k) {
+                if (right-left+1 < min) {
+                    answer[0] = left;
+                    answer[1] = right;
+                    min = right-left+1;
                 }
-                maxIndex++;
-                sum += sequence[maxIndex];
-            } else {
-                if (maxIndex - minIndex < length) {
-                    result[0] = minIndex;
-                    result[1] = maxIndex;
-                    length = maxIndex - minIndex;
-                }
-                sum -= sequence[minIndex];
-                minIndex++;
+                sum -= sequence[left++];
             }
         }
         
-        return result;
+        return answer;
     }
 }
