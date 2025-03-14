7d9c322e4db7aa170229110dd0ffe6427a172c35
Gunwoo Kim <96743351+kimgunwooo@users.noreply.github.com>
2025-03-14 11:32:59 +0900

    [level 2] Title: 큰 수 만들기, Time: 76.88 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+        int cut = number.length() - k;
+        
+        for (int i = 0; i < number.length(); i++) {
+            char digit = number.charAt(i);
+            while (!stack.isEmpty() && k > 0 && stack.peek() < digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        
+        return sb.reverse().substring(0, cut);
+    }
+}
