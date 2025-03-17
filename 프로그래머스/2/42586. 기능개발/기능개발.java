b8b066f6f480c236c639df8b640ed3dc0cad9009
Gunwoo Kim <96743351+kimgunwooo@users.noreply.github.com>
2025-03-17 12:39:03 +0900

    [level 2] Title: 기능개발, Time: 3.37 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,59 @@
+import java.util.*;
+/*
+이전 progresses가 끝나야 다음 프로세스가 가능
+93,1 -> 7
+30,30 -> 3
+55,5 -> 9
+
+7 -> 2
+9 -> 1
+
+95,1 -> 5
+90,1 -> 10
+99,1 -> 1
+99,1 -> 1
+80,1 -> 20
+99,1 -> 1
+
+1, 3, 2
+*/
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            int progress = progresses[i];
+            int speed = speeds[i];
+            
+            int day = (100-progress) / speed;
+            if ((100-progress) % speed != 0) day++;
+            
+            queue.offer(day);
+        }
+        
+        List<Integer> resultList = new ArrayList<>();
+        int idx = 1;
+        int pre = queue.poll();
+        while (!queue.isEmpty()) {
+            int next = queue.poll();
+            System.out.println(next);
+            if (next > pre) {
+                resultList.add(idx);
+                idx = 1;
+                pre = next;
+            } else {
+                idx++;
+            }
+        }
+        
+        resultList.add(idx);
+        
+        int[] answer = new int[resultList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = resultList.get(i);
+        }
+        
+        return answer;
+    }
+}
